4b7ce27f28adcb204d1f7b984399ef0f32b1ebb9
袁尧 <yuanyao@yuanyaodeMacBook-Pro.local>
2018-08-28 12:24:28 +0800

    1.跑通路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {HashRouter as Router,Route} from 'react-router-dom';
+function Root(){
+    return <h1>Root</h1>
+}
+function Home(){
+    return <h1>Home</h1>
+}
+function User(){
+    return <h1>User</h1>
+}
+function Profile(){
+    return <h1>Profile</h1>
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/" component={Root}></Route>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/user" component={User}></Route>
+            <Route path="/profile" component={Profile}></Route>
+        </div>
+    </Router>,
+    document.getElementById('root')
+)
